340e40bb53d07bd49d8c4b7876cc69dc108aa0f1
MrLee5212 <xiaohutuxian5212@163.com>
2014-08-15 09:26:45 +0800

    s

---
+++ src/main/java/com/mybao123/controller/IndexController.java
@@ -0,0 +1,45 @@
+package com.mybao123.controller; 
+  
+import net.sf.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mybao123.model.user.User;
+import com.mybao123.service.IndexService;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+	@Autowired
+	private IndexService indexService;
+	
+	@RequestMapping(value="loadData.do",produces="application/json;charset=UTF-8")
+	public @ResponseBody String LoadData() throws Exception
+	{
+		try
+		{
+			
+			if(indexService != null){
+				indexService.addUser("lrj", "abc");
+			}
+				
+			User u=new User();
+
+			u.setName("��������");
+
+			
+			String json=JSONObject.fromObject(u).toString();
+		
+		
+		return json;
+		}catch(Exception ex)
+		{
+			throw ex;
+		}
+		
+	}	
+} 
