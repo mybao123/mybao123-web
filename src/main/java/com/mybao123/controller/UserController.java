8c0ef39eceb58c00983fdafb4b4d0a26ddb2763f
MrLee5212 <xiaohutuxian5212@163.com>
2014-08-26 16:24:47 +0800

    s

--- src/main/java/com/mybao123/controller/UserController.java
+++ src/main/java/com/mybao123/controller/UserController.java
@@ -29,10 +29,7 @@ public class UserController
 {
 	@Autowired
 	private UserService userService;
-	
-	
-	
-	
+	 
 	/**
 	 * 用户登录
 	 * @param user
@@ -172,8 +169,7 @@ public class UserController
 	private String DecryptUserPwd(String pwd)
 	{ 
 		try
-		{ 
-
+		{  
 			String privateExponet   = "c4f2f4259062203cd1dab960cfe68ffd57399837a8804d58169e01942fc491dddb31ce29b08a6a393bc8378408fda6201280bbd560a0c68177327d1368a4ba1";
 			String modulus  = "88f6ae182d92151ed240e0c7f51136144609dd5ced49f28171ef33af1e3b1d64c7b5939bc0b12e22f3a046168f8890371555e933a2a5714f7b3a858946eea17f";
 			RSAUtils rsa = new RSAUtils(); 
