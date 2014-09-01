8956b36ae386923d5ae0bee1d1bff0d3aa2e590f
MrLee5212 <xiaohutuxian5212@163.com>
2014-09-01 16:09:22 +0800

    s

--- src/main/webapp/senchaTouch/scripts/dataOperate.js
+++ src/main/webapp/senchaTouch/scripts/dataOperate.js
@@ -21,9 +21,11 @@ function loginAction(){
 	}  
 	var module ='88f6ae182d92151ed240e0c7f51136144609dd5ced49f28171ef33af1e3b1d64c7b5939bc0b12e22f3a046168f8890371555e933a2a5714f7b3a858946eea17f';
 	var empoent= '10001';   
-	setMaxDigits(131); //131 => n的十六进制位数/2+3    
+	setMaxDigits(131); //131 => n的十六进制位数/2+3   
+	Ext.Msg.alert('Success', 'setMaxDigits调用不了', Ext.emptyFn); 
     var key  = new RSAKeyPair(empoent, '', module); //10001 => e的十六进制 
     epwd = encryptedString(key, password); 
+	Ext.Msg.alert('Success', '加密调用不了', Ext.emptyFn); 
     var user ={};
     user.name=userName;
     user.psd = epwd;
